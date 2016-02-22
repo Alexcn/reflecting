ea5c1f790248c2913999d50917a5fa59ef0665b0
Light.Zhang <zhilight@gmail.com>
2016-02-22 23:22:57 +0800

    添加忽略文件

--- app/app.py
+++ app/app.py
@@ -2,13 +2,13 @@
 # -*- encoding: utf-8 -*-
 
 from flask import Flask
+from flask import request
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
-
+    user_agent = request.headers.get('User-Agent')
+    return '<p> Your browser is %s</p>' % user_agent
 
 @app.route('/user/<name>')
 def user(name):
