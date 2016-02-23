7a5cc2989fe0813a6b07c8956a58eadd6034ac2b
Light.Zhang <zhilight@gmail.com>
2016-02-23 23:17:40 +0800

    更新路由

--- app/app.py
+++ app/app.py
@@ -3,17 +3,43 @@
 
 from flask import Flask
 from flask import request
+from flask import make_response
+from flask import redirect
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
     return '<p> Your browser is %s</p>' % user_agent
 
+
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
 
+
+@app.route('/bad')
+def bad():
+    return '<h1> Bad Request</h1>', 400
+
+
+@app.route('/cookie')
+def cookie():
+    response = make_response('<h1>This document carries a cookie! </h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+
+@app.route('/user/<id>')
+def get_user(id):
+    pass
+
+
+@app.route('/redirect')
+def redirect_to():
+        return redirect('http://www.sina.com.cn')
 
 if __name__ == '__main__':
     app.run(debug=True)
