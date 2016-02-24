400fe4a061f0bf145d44fcae24fa76ade5eb62ad
Light.Zhang <zhilight@gmail.com>
2016-02-24 23:14:49 +0800

    添加模版以及命令行调试的支持

--- app/app.py
+++ app/app.py
@@ -5,19 +5,19 @@ from flask import Flask
 from flask import request
 from flask import make_response
 from flask import redirect
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p> Your browser is %s</p>' % user_agent
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 
 @app.route('/bad')
