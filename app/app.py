4835fdc4a121de821b99184a4518291fae20ff00
Light.Zhang <zhilight@gmail.com>
2016-03-03 00:18:42 +0800

    实现末班的继承功能

--- app/app.py
+++ app/app.py
@@ -17,6 +17,15 @@ def index():
     return render_template('index.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+        return render_template('500.html'), 500
+
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
