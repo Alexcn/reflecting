631f7d39f10a450add74caa66930bef9644bb910
Light.Zhang <zhilight@gmail.com>
2016-03-07 21:32:54 +0800

    修改默认路由

--- app/app.py
+++ app/app.py
@@ -7,14 +7,18 @@ from flask import make_response
 from flask import redirect
 from flask import render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.errorhandler(404)
