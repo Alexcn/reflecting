299501e9cba13eb40da6b81a86f039e8f6546b04
Light.Zhang <zhilight@gmail.com>
2016-03-07 23:44:46 +0800

    视图函数修改

--- app/app.py
+++ app/app.py
@@ -9,16 +9,31 @@ from flask import render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required])
+    submit = SubmitField('submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.date = ''
+    return render_template('index', form=form, name=name)
+    #return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.errorhandler(404)
