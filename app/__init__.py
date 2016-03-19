2f5075d4a5a5f33ce8a987dc0822a030549861f9
Light.Zhang <zhilight@gmail.com>
2016-03-19 23:29:23 +0800

    程序初始项目

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import config
 
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    return app
