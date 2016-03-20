0853e8e35b3f507f5c2a2c461539239d4efcc89e
Light.Zhang <zhilight@gmail.com>
2016-03-20 21:43:09 +0800

    搭建程序的基本架构

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+from flask import Blueprint
 
+main = Blueprint('main', __name__)
+
+from . import views, errors
+
+
+def create_app(config_name):
+    # ...
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
