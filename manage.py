8e32373bc4c609c57633ee311cdc2330e9e9ad18
Light.Zhang <zhilight@gmail.com>
2016-03-21 22:03:56 +0800

    添加测试命令

--- manage.py
+++ manage.py
@@ -17,6 +17,13 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 
 
+@manager.command
+def test():
+    """Run the unit tests"""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
