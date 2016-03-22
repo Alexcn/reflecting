93f6fdc784862b8c83ecdfc0262c5f93f4c1f018
Light.Zhang <zhilight@gmail.com>
2016-03-22 22:48:20 +0800

    注册认证路由

--- app/__init__.py
+++ app/__init__.py
@@ -25,4 +25,8 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
 
+    # 注册认证路由
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
     return app
