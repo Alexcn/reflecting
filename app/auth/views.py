80263daac44ef028417629152b12883a6e473d9b
Light.Zhang <zhilight@gmail.com>
2016-03-23 21:45:12 +0800

    添加表单处理模块

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, redirect, request, url_for, flash
+from flask.ext.login import login_user
 from . import auth
+from ..models import User
+from .forms import LoginForm
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form)
