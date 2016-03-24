12ab90fd6e5259d125de171b56395835ad14a965
Light.Zhang <zhilight@gmail.com>
2016-03-24 23:53:37 +0800

    添加用户的登出路由

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@
 # -*- encoding: utf-8 -*-
 
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user
+from flask.ext.login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
 from .forms import LoginForm
@@ -18,3 +18,11 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
