8c625fed2799d781e9f3c68f96883838ca4deab7
Light.Zhang <zhilight@gmail.com>
2016-03-27 23:15:38 +0800

    添加用户注册处理逻辑

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,7 +3,8 @@
 
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from ..models import User
 
 
 class LoginForm(Form):
@@ -11,3 +12,18 @@ class LoginForm(Form):
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+
+class RegistrationForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64), Email])
+    username = StringField('Username', validators(Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Username must have only letters,' 'numbers, dots or underscores')]))
+    password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Passwords must match.')])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already resistered')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
