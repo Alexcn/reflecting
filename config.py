370ace75cb4d574944d6271421862ac9cbafcc78
张 志亮 <zhilight@gmail.com>
2016-04-14 13:30:38 +0800

    调整格式

--- config.py
+++ config.py
@@ -4,6 +4,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
