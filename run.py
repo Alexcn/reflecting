8a25c88519d35653bf41952ad3db5bca67cad702
张 志亮 <zhilight@gmail.com>
2016-06-03 16:36:33 +0800

    添加命令行选项

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+
 from app import app
-app.run(debug=True)
+from flask.ext.script import Manager
+manager = Manager(app)
+
+if __name__ == '__main__':
+    manager.run()
