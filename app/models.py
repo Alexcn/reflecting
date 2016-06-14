8dc857978b0d3b4450b6179b02ac3e65ef7fbd70
张 志亮 <zhilight@gmail.com>
2016-06-14 17:49:10 +0800

    添加模型

---
+++ app/models.py
@@ -0,0 +1,39 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+from app import app
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@hostname/database'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Sequence, primary_key=True)
+    name = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+# usertype 类型需要外键关联到 roles 表
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Sequence, primary_key=True)
+    username = db.Column(db.String(64), index=True)
+    usertype = db.Column()
+    birthday = db.Column(db.DATE)
+    address = db.Column(db.String(256))
+    email = db.Column(db.String(64))
+    phone = db.Column(db.CHAR(16))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Sequence, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+    content = db.Column(db.TEXT)
+
