a5e7b1b412dce8edba0e71d28f0876ccbabdb88f
张志亮 <zhilight@gmail.com>
2017-04-26 21:57:45 +0800

    del

--- app/auth/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-
-from . import views
--- app/main/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from flask import Blueprint
-
-main = Blueprint('main', __name__)
-
-from . import views, errors
