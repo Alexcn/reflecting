d36fa1934465e95c6f1c4af47a683dd4512745d0
张志亮 <zhilight@gmail.com>
2017-04-28 16:47:14 +0800

    修改视频

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+
+def index(request):
+    articles = Article.objects.all().values('id')
+    return render(request, 'index.html', articles)
+
+
+def article(request):
+    pass
