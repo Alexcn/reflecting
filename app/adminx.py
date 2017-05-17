55f39d4f7e2d51067972204fbf3a4be32deb5449
张志亮 <zhilight@gmail.com>
2017-05-18 01:19:43 +0800

    完成文章的发布与审核功能

--- app/adminx.py
+++ app/adminx.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import xadmin
-from .models import Article
+from .models import Article, ArticleReview, ArticleApproved
 from xadmin import views
 
 
@@ -16,14 +16,48 @@ class GlobalSettings(object):
 
 
 class ArticleAdmin(object):
-    list_display = ['title', 'short_name']
+    list_display = ['title', ]
     search_fields = ['title', 'short_name']
     list_filter = ['title', 'short_name']
     model_icon = 'fa fa-eyedropper'
     style_fields = {"content": "ueditor"}
 
 
+class ArticleReviewAdmin(object):
+    list_display = ['title', 'passed', 'created_at', 'updated_at', 'go_to']
+    search_fields = ['title', 'created_at']
+    list_filter = ['title', 'created_at']
+    model_icon = 'fa fa-graduation-cap'
+    list_editable = ['passed']
+
+    style_fields = {"content": "ueditor"}
+
+    # 对菜单的列表页面数据进行过滤；
+    def queryset(self):
+        qs = super(ArticleReviewAdmin, self).queryset()
+        qs = qs.filter(passed=False)
+        return qs
+
+
+class ArticleApprovedAdmin(object):
+    list_display = ['title', 'passed', 'created_at', 'updated_at', 'go_to']
+    search_fields = ['title', 'created_at']
+    list_filter = ['title', 'created_at']
+    model_icon = 'fa fa-paper-plane'
+    list_editable = ['passed']
+
+    style_fields = {"content": "ueditor"}
+
+    # 对菜单的列表页面数据进行过滤；
+    def queryset(self):
+        qs = super(ArticleApprovedAdmin, self).queryset()
+        qs = qs.filter(passed=True)
+        return qs
+
+
 xadmin.site.register(Article, ArticleAdmin)
+xadmin.site.register(ArticleReview, ArticleReviewAdmin)
+xadmin.site.register(ArticleApproved, ArticleApprovedAdmin)
 
 xadmin.site.register(views.BaseAdminView, BaseSetting)
 xadmin.site.register(views.CommAdminView, GlobalSettings)
--- app/models.py
+++ app/models.py
@@ -6,25 +6,15 @@ from DjangoUeditor.models import UEditorField
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    passed = models.BooleanField(verbose_name="是否审核通过", default=False)
 
     class Meta:
         abstract = True
 
 
-# class User(BaseModel):
-#     login_name = models.CharField(max_length=32, unique=True, db_index=True)
-#     nick_name = models.CharField(max_length=32, blank=True, null=True)
-#     cell_phone = models.CharField(max_length=24, unique=True, blank=True)
-#     description = models.TextField(max_length=1024, null=True, blank=True)
-#
-#     class Meta:
-#         db_table = 'users'
-
-
 class Article(BaseModel):
     title = models.CharField(max_length=255, default='', verbose_name='文章标题')
     short_name = models.CharField(max_length=255, default='', verbose_name='地址显示')
-    # author = models.CharField(max_length=32, default='', verbose_name='文章作者')
     content = UEditorField(verbose_name='文章内容', width=850, height=400, imagePath="article/ueditor/",
                            filePath="article/ueditor/", default='')
 
@@ -36,3 +26,36 @@ class Article(BaseModel):
         db_table = 'articles'
         verbose_name = '文章撰写'
         verbose_name_plural = verbose_name
+
+
+class ArticleReview(Article):
+    """
+    在 xadmin 中注册不同数据；
+    文章审核
+    """
+    class Meta:
+        verbose_name = '文章审核'
+        verbose_name_plural = verbose_name
+        proxy = True
+
+    def go_to(self):
+        from django.utils.safestring import mark_safe
+        return mark_safe("<a href='https://www.baidu.com'>查看</a>")
+
+    go_to.short_description = "查看"
+
+
+class ArticleApproved(Article):
+    """
+    在 xadmin 中注册不同数据；
+    文章审核通过
+    """
+    class Meta:
+        verbose_name = '已审核文章'
+        verbose_name_plural = verbose_name
+        proxy = True
+
+    def go_to(self):
+        from django.utils.safestring import mark_safe
+        return mark_safe("<a href='http://www.baidu.com'>查看</a>")
+    go_to.short_description = "查看"
