92ea6eb8f9d4f623dfedf2ac5ec22acf74fee418
张志亮 <zhilight@gmail.com>
2017-05-30 23:33:05 +0800

    修改配置

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': 'blog',
         'USER': 'reflecting',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': 5432,
     }
 }
