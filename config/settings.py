2b1e6d4cbb88dfb0948d0128346d769a9f07cead
张志亮 <zhilight@gmail.com>
2017-07-16 16:04:04 +0800

    使用postgres数据库

--- config/settings.py
+++ config/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'July.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog20170716',
+        'USER': 'reflecting',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
