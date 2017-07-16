61beaa0962d7ad4cd803015f5609db1b34fdd6dc
张志亮 <zhilight@gmail.com>
2017-07-17 00:20:12 +0800

    博客模型修改

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -39,6 +39,7 @@ class Article(models.Model):
     body = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
     release_time = models.DateTimeField(default='1970-1-1 00:00:00')
+    updated_time = models.DateTimeField(auto_now=True)
     status = models.CharField(default=0, max_length=1, choices=STATUS_CHOICES)
     read = models.IntegerField(default=0)
     categories = models.ManyToManyField(Categories)
