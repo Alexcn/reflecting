95c5899badd0aed3ef984e78f59d343d48a310be
张志亮 <zhilight@gmail.com>
2017-10-03 15:22:14 +0800

    开发环境事情sqlite3

--- config/settings_dev.py
+++ config/settings_dev.py
@@ -78,24 +78,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog20170716',
-        'USER': 'reflecting',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'blog20170716',
+#         'USER': 'reflecting',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432,
+#     }
+# }
+# 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
