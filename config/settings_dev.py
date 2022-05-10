a07f3c4e5da35fb2ae11168c5747e3ecb187da07
Alex <zhiliang.im@gmail.com>
2022-05-10 23:07:15 +0800

    fix

--- config/settings_dev.py
+++ config/settings_dev.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import sys
+from pathlib import Path
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
